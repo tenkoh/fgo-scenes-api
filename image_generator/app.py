6a1ea1411c8fc64371188368c69f8d2fd7717b17
tenkoh <gardencr+github@gmail.com>
2021-07-04 07:45:29 +0900

    deploy: first version

--- image_generator/app.py
+++ image_generator/app.py
@@ -1,12 +1,19 @@
 import json
 import base64
 from io import BytesIO
+from urllib import parse
 from PIL import Image, ImageDraw, ImageFont
 
-FONT_PATH = "../assets/GenEiLateMin_v2.ttc"
-FONT_RELATIVE_HEIGHT = 0.05
-IMG_PATH = "../assets/plain.png"
+FONT_PATH = "./assets/GenEiLateMin_subset.ttf"
+IMG_PATH = "./assets/plain.png"
+
+# magic numbers
+# result of try&error confirming output image.
 TEXT_MAX_WORDS = 25
+FONT_RELATIVE_HEIGHT = 0.05
+TEXT_ONE_Y = 0.798
+TEXT_TWO_Y = 0.876
+TEXT_X = 0.0708
 
 def insert_text(img, text, relative_xy, fontname, fill="#FFF"):
     """insert text (one line) to Pillow image object
@@ -51,7 +58,7 @@ def pil_to_base64(img):
     img_base64 = base64.b64encode(buf.getvalue()).decode("ascii")
     return img_base64
 
-def lambda_handler():
+def lambda_handler(event, context):
     """
     Parameters
     ----------
@@ -71,20 +78,62 @@ def lambda_handler():
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
-    img = Image.open(IMG_PATH)
-    img = insert_text(img, "今ぐらい幸福にさせろよぅ、", (0,0), FONT_PATH)
-    img = insert_text(img, "そういうところだぞ村正ぁーーーーーーーーーーーーーーーーーーーーーーーーーーーー！", (0,0.1), FONT_PATH)
-    img.save("out.png", "PNG", quality=100, optimize=True)
-
-    img_base64 = pil_to_base64(img)
     
+    try:
+        req = json.loads(event["body"])
+    except TypeError:
+        print("null request body.")
+        return {
+            "statusCode": 400,
+            "body": "null request body." 
+        }
+
+    try:
+        claim = parse.unquote(req["claim"])
+        who = parse.unquote(req["who"])
+    except KeyError:
+        print("unexpected request format")
+        return {
+            "statusCode": 400,
+            "body": "unexpected request format." 
+        }
+
+    try:
+        img = Image.open(IMG_PATH)
+    except:
+        return {
+            "statusCode": 502,
+            "body": "could not open plain image." 
+        }
+
+    try:
+        img = insert_text(img, f"{claim}よぅ、", (TEXT_X, TEXT_ONE_Y), FONT_PATH)
+        img = insert_text(img, f"そういうところだぞ{who}ーーーー！", (TEXT_X, TEXT_TWO_Y), FONT_PATH)
+        # img.save("out.png", "PNG", quality=100, optimize=True)
+    except:
+        return {
+            "statusCode": 502,
+            "body": "could not insert texts." 
+        }
+
+    try:
+        img_base64 = pil_to_base64(img)
+    except:
+        return {
+            "statusCode": 502,
+            "body": "could not encode img by base64" 
+        }
+
     return {
+        "isBase64Encoded": True,
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "image/png",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "https://tenkoh.github.io",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         "body": json.dumps({
             "image": img_base64,
-            # "location": ip.text.replace("\n", "")
         }),
     }
-
-if __name__ == "__main__":
-    lambda_handler()
